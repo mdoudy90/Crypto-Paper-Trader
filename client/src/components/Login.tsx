821fb6d3a72aee7da291fc0c8f24f941b31fbe60
Michael Doudy <michael.doudy@gmail.com>
2020-08-23 17:06:34 -0400

    Refactor Login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
 
-const Login = ({ loginUser, switchView }) => {
-  const [ username, setUsername ] = useState('');
-  const [ password, setPassword ] = useState('');
+interface Props {
+  loginUser: (ev) => void,
+  switchView: (ev) => void
+}
+
+export const Login: React.FC<Props> = ({ loginUser, switchView }) => {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
 
   return (
     <form className='user-form login-form'>
       <h2>LOGIN</h2>
-      <input value = { username } placeholder = { 'Username' } onChange = { (e) => setUsername(e.target.value) }></input>
-      <input value = { password } placeholder = { 'Password' } onChange = { (e) => setPassword(e.target.value) }></input>
-      <button onClick = { (e) => {
+      <input value={username} placeholder={'Username'} onChange={(e) => setUsername(e.target.value)}></input>
+      <input value={password} placeholder={'Password'} onChange={(e) => setPassword(e.target.value)}></input>
+      <button onClick={(e) => {
         e.preventDefault();
         loginUser({ username, password });
         setUsername('');
         setPassword('');
-      } }>Submit</button>
-      <p onClick = { () => { switchView('signup') } }>Sign up now</p>
+      }}>Submit</button>
+      <p onClick={() => { switchView('signup') }}>Sign up now</p>
     </form>
   );
-
 }
-
-export default Login;
