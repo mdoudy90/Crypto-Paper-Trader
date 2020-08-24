41df53e5efd457769528c0b9193f25bda5e8188f
Michael Doudy <michael.doudy@gmail.com>
2020-08-24 12:30:41 -0400

    Refactor ChartView

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import UIDGenerator from 'uid-generator';
 import { Header } from './components/Header';
-import ChartView from './components/ChartView.jsx';
+import ChartView from './components/ChartView';
 import { QuerySelector } from './components/QuerySelector';
 import { StatsView } from './components/StatsView';
 import { Leaderboard } from './components/Leaderboard';
